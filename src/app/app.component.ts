806d68abfb9cb9a964ecd9b2456f1be59590767d
atharvajava <atharvajava@gmail.com>
2017-04-12 00:19:50 +0530

    previewSetup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component,Input,Output } from '@angular/core';
+const app={
+  markdown:"Markdown",
+  preview:"Preview",
+  state:{
+    mark:'Hi'
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  markdown = app.markdown;
+  preview=app.preview;
+  change:any=app.state.mark;
+  handleChange(){
+      this.change=(<HTMLInputElement>document.getElementById("area")).value;
+  }
 }
